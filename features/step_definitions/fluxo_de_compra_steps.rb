bfd6c48febc6647568e6fe68135c355c4c46f20d
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2019-06-16 17:37:05 -0300

    adicionando steps em branco

---
+++ features/step_definitions/fluxo_de_compra_steps.rb
@@ -0,0 +1,19 @@
+Dado('que eu faça um cadstro de um novo usuário') do
+
+end
+
+Quando('eu selecionar um produto no e-commerce') do
+
+end
+
+Quando('adicionar ao carrinho') do
+
+end
+
+Quando('preencher as iformações necessárias') do
+
+end
+
+Então('devo visualizar {string}') do |string|
+
+end
